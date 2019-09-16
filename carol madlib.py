d76aba725c2509b61d193f5a0d198c996f56a145
132-cabdelmalek <55250068+132-cabdelmalek@users.noreply.github.com>
2019-09-16 10:04:52 -0700

    Version 1

---
+++ carol madlib.py
@@ -0,0 +1,25 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> adjective_1 = input("give me an adjective")
+verb_1 = input("give me a verb")
+adjective_2 = input("give me an adjective")
+noun_1 = input("give me a noun")
+verb_2 = input("give me another verb")
+adjective_3 = input("give another adjectuive")
+noun_2 = input('give me a second noun')
+adjective_4 = input("give me a third adjective")
+adjective_5 = input("adjective")
+noun_3 = input("noun")
+noun_4 = input("noun")
+name = input("a person's name: ")
+
+print(" Our school cafeteria has really, " + adjective_1 + "food.")
+print(" Just thinking about it makes my stomach " + verb_1 + " .")
+print(" The spaghetti is " + adjective_2 + "and tastes like " + noun_1 + " .")
+print(" One day, i swear one of my meatballs started to " + verb_2 + " !")
+print(" The turkey tacos are totally " + adjective_3 + "and look kind of like old " + noun_2 + ".")
+print(" My friend " + name + " actually likes the meatloaf, even thought it's " + adjective_4 + " and " + adjective_5 + " .")
+print(" I call it 'mystery meatloaf' and think it's really made out of " + noun_3 + " .")
+print(" My dad said he'd make my lunches, but the first day, he made me a sandwich out of " + noun_4 + " and peanut butter! I think I'd rather take my chances with the cafeteria! ")
+
+
