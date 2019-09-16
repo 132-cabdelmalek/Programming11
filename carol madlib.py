3ba2cf0dd0e98dfd82dc065b95ef34c98957cd8c
132-cabdelmalek <55250068+132-cabdelmalek@users.noreply.github.com>
2019-09-16 10:18:26 -0700

    Update carol madlib.py

--- carol madlib.py
+++ carol madlib.py
@@ -1,7 +1,5 @@
 #githubversion
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> adjective_1 = input("give me an adjective")
+adjective_1 = input("give me an adjective")
 verb_1 = input("give me a verb")
 adjective_2 = input("give me an adjective")
 noun_1 = input("give me a noun")
